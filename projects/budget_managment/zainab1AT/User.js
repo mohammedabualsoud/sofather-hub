4b119c7bf63a23919f40242d55e58cd4576b78ba
zainab <engzainabatwa1@gmail.com>
2024-08-13 13:34:04 +0300

    BudgetManagement

--- projects/budget_managment/zainab1AT/User.js
+++ projects/budget_managment/zainab1AT/User.js
@@ -1,18 +1,19 @@
 import BudgetManagementService from "./BudgetManagementService.js";
 export default class User {
-  constructor(userName, firstName, lastName) {
-    this.userName = userName;
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.balance = 0;
+  #userName;
+  #firstName;
+  #lastName;
+  #balance;
+
+   constructor(userName, firstName, lastName) {
+    this.#userName = userName;
+    this.#firstName = firstName;
+    this.#lastName = lastName;
+    this.#balance = 0;
   }
 
   setBalance(newBalance) {
     this.balance = newBalance;
-  }
-
-  setBalance(newBalance) {
-    this.#balance = newBalance;
   }
 
   getUsername() {
