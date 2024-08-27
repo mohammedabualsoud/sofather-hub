09284112540f5dbbea399f12aed07f126ace2173
zainab <engzainabatwa1@gmail.com>
2024-08-27 13:10:40 +0300

    "Updated .gitignore to include .env file, modified mysql.js to use environment variables, and added dotenv dependency to package.json and package-lock.json"

--- projects/budget_managment/zainab1AT/mysql.js
+++ projects/budget_managment/zainab1AT/mysql.js
@@ -1,11 +1,13 @@
 import mysql from "mysql";
 import util from "util";
+import dotenv from 'dotenv';
 
+dotenv.config(); 
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "budgetmanagement_db",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 con.query = util.promisify(con.query).bind(con);
