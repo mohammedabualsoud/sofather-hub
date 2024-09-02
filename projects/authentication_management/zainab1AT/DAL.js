0119c3e44d2b827e7d2204be4c4067427a314669
zainab <engzainabatwa1@gmail.com>
2024-09-02 23:56:44 +0300

    Set up basic authentication: Added DAL class, MySQL connection, and implemented signup/login functionality.

---
+++ projects/authentication_management/zainab1AT/DAL.js
@@ -0,0 +1,23 @@
+export default class DAL {
+  constructor(con) {
+    this.con = con;
+  }
+  async findByUsername(username) {
+    const [user] = await this.con.query(
+      "SELECT * FROM USER WHERE username = ?",
+      [username]
+    );
+    return user;
+  }
+
+  async addUser(username, email, password, role) {
+    await this.con.query(
+      `INSERT INTO USER(username,email,password,role)
+       VALUES(?,?,?,?)
+            ON DUPLICATE KEY UPDATE
+            email = VALUES(email),
+            role = VALUES(role)`,
+      [username, email, password, role]
+    );
+  }
+}
