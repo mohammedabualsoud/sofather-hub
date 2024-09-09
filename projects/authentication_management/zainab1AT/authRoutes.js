7f9f843484cc2315219498704b3bd387b1915d7b
zainab <engzainabatwa1@gmail.com>
2024-09-09 23:51:10 +0300

    Updated authRoutes.js and userRoutes.js files in authentication_management project

--- projects/authentication_management/zainab1AT/authRoutes.js
+++ projects/authentication_management/zainab1AT/authRoutes.js
@@ -2,10 +2,13 @@ import express from "express";
 import bcrypt from "bcrypt";
 import DAL from "./DAL.js";
 import { getConnection } from "./mysql.js";
-
 const router = express.Router();
 const con = getConnection();
 const DALInstance = new DAL(con);
+
+export async function isValidPassword(pass, hashPass) {
+  return await bcrypt.compare(pass, hashPass);
+}
 
 router.post("/signup", async (req, res) => {
   const { username, email, password, role } = req.body;
@@ -22,12 +25,17 @@ router.post("/login", async (req, res) => {
   const { username, password } = req.body;
   try {
     const user = await DALInstance.findByUsername(username);
-    if (user) {
-      const match = await bcrypt.compare(password, user.password);
-      if (match)
-        res.status(200).json({ message: "User logged in successfully" });
-      else res.status(401).json({ message: "Invalid username or password" });
-    } else res.status(401).json({ message: "Invalid username or password" });
+    if (!user) {
+      res.status(401).json({ error: "Authentication failed" });
+      return;
+    }
+
+    if (!isValidPassword(password, user.password)) {
+      res.status(401).json({ error: "Authentication failed" });
+      return;
+    }
+
+    res.status(200).json({message:"Login successful"});
   } catch (error) {
     res.status(500).json({ error: "Login failed" });
   }
